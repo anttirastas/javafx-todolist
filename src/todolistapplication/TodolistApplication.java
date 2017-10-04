1e3e90f6d969b841ce0174bf04ed7d2b2a266963
Antti Rastas <antti.rastas@gmail.com>
2017-10-04 14:49:19 +0300

    Add HBox for top buttons, add top buttons (create task and create category), create GridPane for actual list, attach these elements to root BorderPane, Scene, and Stage

--- src/todolistapplication/TodolistApplication.java
+++ src/todolistapplication/TodolistApplication.java
@@ -6,7 +6,12 @@
 package todolistapplication;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import todolistapplication.domain.TodoList;
 
@@ -23,13 +28,29 @@ public class TodolistApplication extends Application {
         TodoList todolist = new TodoList();
         
         // Create HBox to contain action buttons
+        HBox hbox = new HBox();
+        Button addTaskButton = new Button("Add task");
+        Button addCategoryButton = new Button("Add category");
+        
+        hbox.getChildren().addAll(addTaskButton, addCategoryButton);
+        
         
         // Create GridPane to contain actual todolist
+        GridPane gridPane = new GridPane();
+        for (int i = 0; i < todolist.getTasks().size(); i++) {
+            Label task = new Label(todolist.getTasks().get(i).getName());
+        }
+        
         
         // Add appropriate event handlers
         
         // Add everything to same layout and show stage
+        root.setTop(hbox);
+        root.setCenter(gridPane);
         
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
         
     }
     
