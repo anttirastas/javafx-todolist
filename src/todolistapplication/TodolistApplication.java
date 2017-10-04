1177bb1a3f0cad2a1a1556b358ba6d5276af4f34
Antti Rastas <antti.rastas@gmail.com>
2017-10-04 14:26:49 +0300

    Create skeleton of TodoListApplication class with empty BorderPane as root and a new (empty) TodoList instance

--- src/todolistapplication/TodolistApplication.java
+++ src/todolistapplication/TodolistApplication.java
@@ -5,17 +5,41 @@
  */
 package todolistapplication;
 
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import todolistapplication.domain.TodoList;
+
 /**
  *
  * @author antti
  */
-public class TodolistApplication {
+public class TodolistApplication extends Application {
 
     /**
      * @param args the command line arguments
      */
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane root = new BorderPane();
+        
+        TodoList todolist = new TodoList();
+        
+        // Create HBox to contain action buttons
+        
+        // Create GridPane to contain actual todolist
+        
+        // Add appropriate event handlers
+        
+        // Add everything to same layout and show stage
+        
+        
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(TodolistApplication.class);
+        
     }
     
 }
